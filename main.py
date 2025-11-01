849813108f45a00c7c4fb538567933920daf1e1b
Raja5736 <huzaifa573123@gmail.com>
2025-11-01 10:30:41 +0500

    initial commit

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 
 for i in range(1,13):
     print("11*%d=%d"%(i,11*i))
+
+num=[1,2,3,4,7,6]
+print(num)
+
+for i in range(0,len(num)):
+    print(num[i])
+
+
+    
+               
---
+++ while.py
@@ -0,0 +1,11 @@
+#Write a Python program for printing the sum of the first ten natural numbers using the while loop.
+sum=0
+a=1
+while a<=10:
+    sum=sum+a
+    a=a+1
+print("The sum of first 10 natural numbers is",sum)    
+
+
+
+
